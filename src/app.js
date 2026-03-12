8e1b7ed2703927dc039dadaf8ad8570e94e5fc6c
Yerniyaz <t.yerniyaz@gmail.com>
2026-03-12 19:13:45 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: ('*'),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
